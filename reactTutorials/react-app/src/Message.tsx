e1f3de16e6231ba01b1bdda0112e71818b77da62
minhyeong <minhyeong9986@gmail.com>
2025-03-18 16:28:31 -0500

    React Tutorials

---
+++ reactTutorials/react-app/src/Message.tsx
@@ -0,0 +1,10 @@
+// PascalCasing - A convention Capitalize every first letter
+function Message() {
+    // JSX: JavaScript XML - JSX can easily create dynamic content
+    const name = '';
+    if (name)
+        return <h1>Hello {name}</h1>;
+    return <h1>Hello World</h1>;
+}
+
+export default Message;
